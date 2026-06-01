ffe890a372ac794a0155469f39e6570778911061
Divyanshu Kumar Jha <divyanshukumarjha47@gmail.com>
2026-06-01 14:04:18 +0530

    refactor: simplify projects subpage to basic placeholder h3 skeleton

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,7 @@
 export default function Projects() {
-    return (
-        <div>
-            <h1>Projects</h1>
-        </div>
-    )
+  return (
+    <div className="py-20 text-center select-none">
+      <h3 className="text-2xl text-slate-400 font-semibold font-display">Projects Page</h3>
+    </div>
+  )
 }
