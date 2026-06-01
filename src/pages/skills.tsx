d4cd5eb0377ceef7380894f1d09068e91245e1c3
Divyanshu Kumar Jha <divyanshukumarjha47@gmail.com>
2026-06-01 14:04:14 +0530

    refactor: simplify skills subpage to basic placeholder h3 skeleton

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,7 +1,7 @@
 export default function Skills() {
-    return (
-        <div>
-            <h1>Skills</h1>
-        </div>
-    )
+  return (
+    <div className="py-20 text-center select-none">
+      <h3 className="text-2xl text-slate-400 font-semibold font-display">Skills Page</h3>
+    </div>
+  )
 }
